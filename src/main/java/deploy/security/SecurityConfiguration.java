f69eb8cd203a3fadf474df4d6e7cebb669789ade
rashmi <rashmi>
2020-05-23 22:11:29 +0530

    refactoring

--- src/main/java/deploy/security/SecurityConfiguration.java
+++ src/main/java/deploy/security/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package deploy;
+package deploy.security;
 
 import javax.sql.DataSource;
 
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 
 //@Configuration
 //@EnableWebSecurity
